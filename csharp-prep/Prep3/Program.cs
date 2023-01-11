ba192b71aef6077a83ed949281413ce37a45e378
Shrimppa <trentonringger@gmail.com>
2023-01-11 16:57:16 -0700

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int userGuess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userGuessString = Console.ReadLine();
+            userGuess = int.Parse(userGuessString);
+
+            if (userGuess != magicNumber)
+            {
+                if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (userGuess != magicNumber);
     }
 }
+
+
