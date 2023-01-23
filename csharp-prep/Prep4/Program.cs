72019b13c427d2ada1ead8f0cf2966d7c61508c1
Shrimppa <trentonringger@gmail.com>
2023-01-23 11:58:55 -0700

    stuff

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int number = 1;
+        int sum = 0;
+        int increment = 0;
+        int largestNumber = 0;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            sum = sum + number;
+            increment = increment + 1;
+            numbers.Add(number); // You can do whatever you want with this list of numbers although I did all previous calculations without a list
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+        }
+        float average = 0;
+        average = sum / increment;
+        Console.WriteLine($"The sum is: {sum}\nThe average is: {average}\nThe largest number is: {largestNumber}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userInput = Console.ReadLine();
+        return userInput;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        int userNumber = int.Parse(userInput);
+        return userNumber;
+    }
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+        return squared;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
