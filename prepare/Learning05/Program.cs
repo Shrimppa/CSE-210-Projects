302ffe25ecd83e276b62f5dfec5be27c5ce8fcbb
Shrimppa <trentonringger@gmail.com>
2023-03-05 22:32:13 -0700

    Finished Learning 5.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Purple", 63);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 7, 34);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("White", 75);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
